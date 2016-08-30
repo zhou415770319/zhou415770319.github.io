2fbd0364c326c66875d50d9ecdc16c2fdd449597
zhoufei <415770319@qq.com>
2016-08-30 14:51:38 +0800

    push

--- wordpress/wp-config-sample.php
+++ wordpress/wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'feiyuboke');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'username_here');
+define('DB_USER', 'feiyu');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'zhou9468334');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
